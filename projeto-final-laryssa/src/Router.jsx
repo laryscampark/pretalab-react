9a41ae56a486e7e41f3efd178b9e45093d149b1d
Laryssa Peçanha <laryssascampark@gmail.com>
2022-12-09 14:02:58 -0300

    ligando as rotas e estilizando a pagina inicial

--- projeto-final-laryssa/src/Router.jsx
+++ projeto-final-laryssa/src/Router.jsx
@@ -1,14 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import { Inicio } from './pages/Inicio'
+
+import { Menu } from './components/Menu'
+
+import { Footer } from './components/Footer'
+
 export function Router() {
     return (
         <BrowserRouter>
+            <Menu />
             <Routes>
-                <Route path="/" element={<h1>Inicio</h1>}/>
+                <Route path="/" element={<Inicio />}/>
                 <Route path="/sobre" element={<h1>Sobre</h1>}/>
                 <Route path="/portifolio" element={<h1>Portifolio</h1>}/>
                 <Route path="/contato" element={<h1>contato</h1>}/>
             </Routes>
+            <Footer />
         </BrowserRouter>
     )
 }
