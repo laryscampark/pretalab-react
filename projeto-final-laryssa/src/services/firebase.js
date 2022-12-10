faf430a8dd38b6327042199dc70b1b8f6e34116e
Laryssa Peçanha <laryssascampark@gmail.com>
2022-12-10 20:21:16 -0300

    ajuste do banco de dados

--- projeto-final-laryssa/src/services/firebase.js
+++ projeto-final-laryssa/src/services/firebase.js
@@ -8,12 +8,13 @@ import { getDatabase } from 'firebase/database'
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  databaseURL: import.meta.env.VITE_DATABASE_URL
+  appId: import.meta.env.VITE_APP_ID
 }
+
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
