a732c15a0f9cb9f162d5f4b6dc6b4ead76d13fab
Laryssa Peçanha <laryssascampark@gmail.com>
2022-12-09 21:22:39 -0300

    criação do formulario da página de contato

--- projeto-final-laryssa/src/pages/Inicio.jsx
+++ projeto-final-laryssa/src/pages/Inicio.jsx
@@ -2,13 +2,15 @@ import { Student, BracketsCurly, AddressBook } from 'phosphor-react'
 import { Header } from '../components/Header'
 import homeImg from '../assets/home.svg'
 
+import styles from '../styles/pages/inicio.module.css'
+
 export function Inicio() {
     return (
         <>
             <Header text="Meu site pessoal" image={homeImg} />
             <div className={styles.homeContainer}>
                 <div className={styles.cardContainer}>
-                    <Student size={150} weight="thin" />
+                    <Student size={150} color="#d180bd" weight="thin" />
                     <h2 className={styles.cardTitle}>Vida</h2>
                     <p className={styles.cardText}>Estudante de Sistemas de Informação na FAETERJ PARACAMBI, moro em Queimados - RJ</p>
                 </div>
