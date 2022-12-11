60a2b7f5fa45369d99b6b33ef9c50bc1a85631d1
Laryssa Peçanha <laryssascampark@gmail.com>
2022-12-10 23:13:06 -0300

    ajuste do banco de dados e estilização da pagina portifolio

--- projeto-final-laryssa/src/pages/Github.jsx
+++ projeto-final-laryssa/src/pages/Github.jsx
@@ -2,12 +2,12 @@ import { useEffect, useState } from "react";
 import styles from '../styles/pages/portifolio.module.css'
 
 export function Github() {
-    const [ repositories, setRepositories ] = useState([])
+    const [repositories, setRepositories] = useState([])
 
     useEffect(() => {
         fetch('https://api.github.com/users/laryscampark/repos')
-        .then(response => response.json())
-        .then(data => setRepositories(data))
+            .then(response => response.json())
+            .then(data => setRepositories(data))
     }, [])
 
     return (
@@ -18,7 +18,16 @@ export function Github() {
                         <li className={styles.cardRepo}>
                             <h3 className={styles.cardRepoText}>{repository.name}</h3>
                             <p className={styles.cardRepoText}>{repository.description}</p>
-                            <a href={repository.html_url} target="_blank" rel="noreferrer">Saiba mais</a>
+                            <a href={repository.html_url} target="_blank" rel="noreferrer">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff"
+                                    viewBox="0 0 256 256"><rect width="256" height="256" fill="none">
+                                    </rect>
+                                    <polyline points="176 104 224 152 176 200"
+                                        fill="none" stroke="#fff" stroke-linecap="round"
+                                        stroke-linejoin="round" stroke-width="8">
+                                    </polyline><path d="M32,56a96,96,0,0,0,96,96h96" fill="none"
+                                        stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="8">
+                                    </path></svg></a>
                         </li>
                     )
                 })}
--- projeto-final-laryssa/src/pages/Portifolio.jsx
+++ projeto-final-laryssa/src/pages/Portifolio.jsx
@@ -8,10 +8,30 @@ export function Portifolio() {
     return (
         <>
             <Header text="Meus projetinhos" image={portifolioImg} />
-                <div>
-                    <h2 className={styles.projectsTitle}>Meus projetos no Github</h2>
-                    <Github></Github>
+            <div>
+                <h2 className={styles.projectsTitle}>Favoritos</h2>
+                <div class={styles.cardsContainer}>
+                    <div class={styles.card}>
+                        <h1 className={styles.projectsContainer}>carzinho</h1>
+                        <img class={styles.cardImage} src="https://media.giphy.com/media/j5WLPN5pofMBurAynh/giphy.gif" />
+                        <p className={styles.projectsContainer}>Criação de um cardzinho com codigo NFT</p>
+                        <a class={styles.cardLink} href="https://cardzinho.netlify.app/" target="_blank" rel="noreferrer">Ver projeto</a>
+                    </div>
+                    <div class={styles.card}>
+                        <h1 className={styles.projectsContainer}>Jogo do Cavaleiro</h1>
+                        <img class={styles.cardImage} src="https://media.giphy.com/media/e9AQhFdtkF7UEfexbB/giphy.gif" />
+                        <p className={styles.projectsContainer}>Jogo de RPG de cavaleiro foi criado com JavaScript se encostar nos mostrinhos jogo reinicia</p>
+                        <a class={styles.cardLink} href="https://jogodocavaleiro.netlify.app/" target="_blank" rel="noreferrer">Ver projeto</a>
+                    </div><div class={styles.card}>
+                        <h1 className={styles.projectsContainer}>Meu Portfolio</h1>
+                        <img class={styles.cardImage }src="https://media.giphy.com/media/RaT2tOOlt1WPIb7sGA/giphy.gif" />
+                        <p className={styles.projectsContainer}>Um site em react para demonstrar meu portfolio</p>
+                        <a class={styles.cardLink} href="https://portfolio-aula-react.netlify.app/" target="_blank" rel="noreferrer">Ver projeto</a>
+                    </div>
                 </div>
+                <h2 className={styles.projectsTitle}>Meus projetos no Github</h2>
+                <Github></Github>
+            </div>
         </>
     )
 
