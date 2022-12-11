70cd009187be7cb9aa03ed904d6f256d52e6bc8a
Laryssa Peçanha <laryssascampark@gmail.com>
2022-12-10 22:20:12 -0300

    estilizando a página portifolio

--- projeto-final-laryssa/src/pages/Github.jsx
+++ projeto-final-laryssa/src/pages/Github.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import styles from '../styles/pages/portifolio.module.css'
 
 export function Github() {
     const [ repositories, setRepositories ] = useState([])
@@ -10,14 +11,14 @@ export function Github() {
     }, [])
 
     return (
-        <div>
-            <ul>
+        <div className={styles.projectsContainer}>
+            <ul className={styles.cardsRepoContainer}>
                 {repositories.map(repository => {
                     return (
-                        <li>
-                            <h3>{repository.name}</h3>
-                            <p>{repository.description}</p>
-                            <a href={repository.html_url} target="_blank">Saiba mais</a>
+                        <li className={styles.cardRepo}>
+                            <h3 className={styles.cardRepoText}>{repository.name}</h3>
+                            <p className={styles.cardRepoText}>{repository.description}</p>
+                            <a href={repository.html_url} target="_blank" rel="noreferrer">Saiba mais</a>
                         </li>
                     )
                 })}
--- projeto-final-laryssa/src/pages/Portifolio.jsx
+++ projeto-final-laryssa/src/pages/Portifolio.jsx
@@ -2,12 +2,14 @@ import { Header } from "../components/Header";
 import { Github } from './Github'
 import portifolioImg from '../assets/portifolio.svg'
 
+import styles from '../styles/pages/portifolio.module.css'
+
 export function Portifolio() {
     return (
         <>
             <Header text="Meus projetinhos" image={portifolioImg} />
                 <div>
-                    <h2>Meus projetos no Github</h2>
+                    <h2 className={styles.projectsTitle}>Meus projetos no Github</h2>
                     <Github></Github>
                 </div>
         </>
