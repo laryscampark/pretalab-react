4bb92d9853131cb8b4b48c4381d9b1cf34ab5f83
Laryssa Peçanha <laryssascampark@gmail.com>
2022-12-10 23:53:35 -0300

    finalização da pagina portifolio

--- projeto-final-laryssa/src/pages/Portifolio.jsx
+++ projeto-final-laryssa/src/pages/Portifolio.jsx
@@ -24,7 +24,7 @@ export function Portifolio() {
                         <a class={styles.cardLink} href="https://jogodocavaleiro.netlify.app/" target="_blank" rel="noreferrer">Ver projeto</a>
                     </div><div class={styles.card}>
                         <h1 className={styles.projectsContainer}>Meu Portfolio</h1>
-                        <img class={styles.cardImage }src="https://media.giphy.com/media/RaT2tOOlt1WPIb7sGA/giphy.gif" />
+                        <img class={styles.cardImage }src="https://media.giphy.com/media/VUpOoIMiubYJnsujSD/giphy.gif" />
                         <p className={styles.projectsContainer}>Um site em react para demonstrar meu portfolio</p>
                         <a class={styles.cardLink} href="https://portfolio-aula-react.netlify.app/" target="_blank" rel="noreferrer">Ver projeto</a>
                     </div>
