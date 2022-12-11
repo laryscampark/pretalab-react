9af391a4aef72c1be0791099198b6df447cc0bc7
Laryssa Peçanha <laryssascampark@gmail.com>
2022-12-11 00:31:15 -0300

    ajuste finais

--- projeto-final-laryssa/src/pages/Portifolio.jsx
+++ projeto-final-laryssa/src/pages/Portifolio.jsx
@@ -14,13 +14,13 @@ export function Portifolio() {
                     <div class={styles.card}>
                         <h1 className={styles.projectsContainer}>carzinho</h1>
                         <img class={styles.cardImage} src="https://media.giphy.com/media/j5WLPN5pofMBurAynh/giphy.gif" />
-                        <p className={styles.projectsContainer}>Criação de um cardzinho com codigo NFT</p>
+                        <p className={styles.projectsContainer}>Uma aplicação com HTML/CSS para fazer um cardzinho com codigo NFT</p>
                         <a class={styles.cardLink} href="https://cardzinho.netlify.app/" target="_blank" rel="noreferrer">Ver projeto</a>
                     </div>
                     <div class={styles.card}>
                         <h1 className={styles.projectsContainer}>Jogo do Cavaleiro</h1>
                         <img class={styles.cardImage} src="https://media.giphy.com/media/e9AQhFdtkF7UEfexbB/giphy.gif" />
-                        <p className={styles.projectsContainer}>Jogo de RPG de cavaleiro foi criado com JavaScript se encostar nos mostrinhos jogo reinicia</p>
+                        <p className={styles.projectsContainer}>Um jogo em JavaScript com obstáculos onde se movimenta pelas setas do teclado do computador e tem um cavaleiro não pode enconstar nos monstro</p>
                         <a class={styles.cardLink} href="https://jogodocavaleiro.netlify.app/" target="_blank" rel="noreferrer">Ver projeto</a>
                     </div><div class={styles.card}>
                         <h1 className={styles.projectsContainer}>Meu Portfolio</h1>
