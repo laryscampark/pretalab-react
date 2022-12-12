179b973058019951839aed3525178113c00b36fb
Laryssa Peçanha <laryssascampark@gmail.com>
2022-12-12 10:19:11 -0300

    atualização dos projetos favoritos na pagina do portifolio

--- projeto-final-laryssa/src/pages/Portifolio.jsx
+++ projeto-final-laryssa/src/pages/Portifolio.jsx
@@ -13,18 +13,18 @@ export function Portifolio() {
                 <div class={styles.cardsContainer}>
                     <div class={styles.card}>
                         <h1 className={styles.projectsContainer}>carzinho</h1>
-                        <img class={styles.cardImage} src="https://media.giphy.com/media/j5WLPN5pofMBurAynh/giphy.gif" />
+                        <img class={styles.cardImage} src="https://media.giphy.com/media/aAZoUDyWJGaWIyT3G6/giphy.gif" />
                         <p className={styles.projectsContainer}>Uma aplicação com HTML/CSS para fazer um cardzinho com codigo NFT</p>
                         <a class={styles.cardLink} href="https://cardzinho.netlify.app/" target="_blank" rel="noreferrer">Ver projeto</a>
                     </div>
                     <div class={styles.card}>
                         <h1 className={styles.projectsContainer}>Jogo do Cavaleiro</h1>
-                        <img class={styles.cardImage} src="https://media.giphy.com/media/e9AQhFdtkF7UEfexbB/giphy.gif" />
-                        <p className={styles.projectsContainer}>Um jogo em JavaScript com obstáculos onde se movimenta pelas setas do teclado do computador e tem um cavaleiro não pode enconstar nos monstro</p>
+                        <img class={styles.cardImage} src="https://media.giphy.com/media/3gDWzyoLaP92P9vYST/giphy.gif" />
+                        <p className={styles.projectsContainer}>Projeto construído no evento da HeroKu com JavaScript</p>
                         <a class={styles.cardLink} href="https://jogodocavaleiro.netlify.app/" target="_blank" rel="noreferrer">Ver projeto</a>
                     </div><div class={styles.card}>
                         <h1 className={styles.projectsContainer}>Meu Portfolio</h1>
-                        <img class={styles.cardImage }src="https://media.giphy.com/media/VUpOoIMiubYJnsujSD/giphy.gif" />
+                        <img class={styles.cardImage }src="https://media.giphy.com/media/R7zVqi0BL6ngehLccm/giphy.gif" />
                         <p className={styles.projectsContainer}>Um site em react para demonstrar meu portfolio</p>
                         <a class={styles.cardLink} href="https://pretalab-projeto-final.vercel.app/" target="_blank" rel="noreferrer">Ver projeto</a>
                     </div>
